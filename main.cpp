c1b5c02f4b0815c2ee418756eac799ac1cbf8533
Sepehr Noroozi <noroozi.sepehr@gmail.com>
2025-05-07 11:40:53 +0330

    Task class and its functions added. function prototypes added.

--- main.cpp
+++ main.cpp
@@ -6,7 +6,46 @@
 
 using namespace std;
 
+
+
+//Task class definition
+class Task{
+private:
+    string description;
+    bool isDone;
+public:
+    //Constructor to initialize task with a description, isDone defaults to false
+    Task(string desc) : description(desc), isDone(false){}
+
+    //Getter for description
+    string getDescription() const{
+        return description;
+    }
+
+    //Getter for isDone status
+    bool getIsDone() const{
+        return isDone;
+    }
+
+    //Mark task as done
+    void markAsDone() {
+        isDone = true;
+    }
+
+    //Setter for description (in case you want to change task description)
+    void setDescription(const string& desc) {
+        description = desc;
+    }
+};
+
+//Function prototypes
+void displayMenu();
+void addTask(vector<Task>& tasks);
+void viewTasks(const vector<Task>& tasks);
+void markTaskAsDone(vector<Task>& tasks);
+void deleteTask(vector<Task>& tasks);
+
 int main()
 {
-
+    vector<Task> tasks;
 }
